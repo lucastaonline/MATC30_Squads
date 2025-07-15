628b13d5dabe45cee51659333d7f08b9a7f2f044
Arnaldo Liro <dinholiro@gmail.com>
2025-07-15 14:57:01 -0300

    fix: nome do form

--- rankedor-de-links/frontend/components/Form/index.tsx
+++ rankedor-de-links/frontend/components/Form/index.tsx
@@ -6,7 +6,7 @@ export default function Form() {
     return(
         <div className="rounded-lg flex justify-center items-center min-h-screen bg-gradient-to-br from-green-50 to-blue-50 font-sans">
             <div className="bg-white rounded-2xl shadow-lg text-black p-8 min-w-[350px] w-full max-w-md border border-gray-100">
-                <h1 className="font-bold text-2xl text-center mb-7 tracking-tight">Digite os dados da sua empresa:</h1>
+                <h1 className="font-bold text-2xl text-center mb-7 tracking-tight">Digite os dados do site:</h1>
                 <form className="flex flex-col gap-4 w-full">
                     <div className="flex flex-col items-start w-full gap-2">
                         <FormInput />
