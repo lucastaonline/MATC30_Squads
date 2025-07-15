db2b1a6408590a6ab0e685449bea3d12485d090b
Arnaldo Liro <dinholiro@gmail.com>
2025-07-15 10:49:39 -0300

    feat: criando form e estilizando botão

--- rankedor-de-links/frontend/components/Form/index.tsx
+++ rankedor-de-links/frontend/components/Form/index.tsx
@@ -2,9 +2,15 @@ import FormButton from "../FormButton"
 
 export default function Form() {
     return(
-        <form action="submit" className="">
-            <label>Teste</label>
-            <input type="text" />
-        </form>
+        <div className="bg-[#ccc] rounded-sm shadow-sm shadow-white text-black p-2">
+            <h1 className="text-center mb-5">Digite os dados da sua empresa:</h1>
+            <form className="text-center">
+                <div className="flex gap-2 mb-5">
+                    <label>Teste</label>
+                    <input type="text" className="outline-none border rounded-md"/>
+                </div>
+                <FormButton />
+            </form>
+        </div>
     )
 }
--- rankedor-de-links/frontend/components/FormButton/index.tsx
+++ rankedor-de-links/frontend/components/FormButton/index.tsx
@@ -1,6 +1,6 @@
 export default function FormButton() {
     return (
-        <button>
+        <button className="border rounded-md py-1 px-3 cursor-pointer hover:shadow-md shadow-[#fff] hover:text-white text-bold hover:border-white hover:bg-green-500 scale-85 hover:scale-105 transition-all duration-300">
             Enviar
         </button>
     )
