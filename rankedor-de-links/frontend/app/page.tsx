f644813b19cba9dd074fbdbf82994c784d51ce1d
iago oliveira nascimento <iago.onascimento@hotmail.com>
2025-07-21 00:14:08 -0300

    colocando campo

--- rankedor-de-links/frontend/app/page.tsx
+++ rankedor-de-links/frontend/app/page.tsx
@@ -3,5 +3,10 @@ export default function Home() {
                 <div style={{marginTop: "12rem", marginBottom: "10rem"}} >
                     <h1>Bem vindo ao sistema!</h1>
                 </div>
+                <div style={{ width: "26rem" }} >
+                    <input type="text" id="first_name" 
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            placeholder="Seu nome aqui"/>
+                </div>
             </div>);
 }
